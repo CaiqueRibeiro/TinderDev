199560fd2bf4e47c9a6737345f4bc0430138c2a3
caiqueribeiro <caique.rodrigues2@fatec.sp.gov.br>
2019-08-10 15:58:44 -0300

    Adicionando CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 // cria o servidor com a função do express
 const server = express();
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://caiqueribeiro:caiqueribeiro@cluster0-xhvf1.mongo
     useNewUrlParser: true
 });
 
+// Para receber requisições cross-platform
+server.use(cors());
 // para entender requisições com corpo JSON
 server.use(express.json());
 // para usar as rotas definidas
